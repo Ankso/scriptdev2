c81f3d9b5948aff423edc9161e7b1235224b0450
Ankso <ankso@reinodelaoscuridad.com>
2010-08-30 17:50:19 +0200

    Complete Light of Down just before event starts (see comments).

--- scripts/eastern_kingdoms/scarlet_enclave/ebon_hold.cpp
+++ scripts/eastern_kingdoms/scarlet_enclave/ebon_hold.cpp
@@ -2229,6 +2229,16 @@ struct MANGOS_DLL_DECL npc_highlord_darion_mograineAI : public npc_escortAI
                     case 3: // rise
                         DoScriptText(SAY_LIGHT_OF_DAWN05, m_creature);
                         JumpToNextStep(3000);
+                        // Sometimes quest gets bugged and players can't continue with it, so complete quest now,
+                        // and lets player chose if he/she continues the quest or if he ignores the event.
+                        Map *map = m_creature->GetMap(); // search players with in 50 yards for quest credit
+                        Map::PlayerList const &PlayerList = map->GetPlayers();
+                        if (!PlayerList.isEmpty())
+                        {
+                            for (Map::PlayerList::const_iterator i = PlayerList.begin(); i != PlayerList.end(); ++i)
+                                 if (i->getSource()->isAlive() && m_creature->IsWithinDistInMap(i->getSource(), 50))
+                                     i->getSource()->CastSpell(i->getSource(), SPELL_THE_LIGHT_OF_DAWN_Q, false);
+                        }
                         break;
 
                     case 4: // summon ghoul
