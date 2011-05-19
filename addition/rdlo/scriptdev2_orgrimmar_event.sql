90637c087f6d86d87e6e858871b152ea81fb0dc5
Ankso <administrador.ankso@reinodelaoscuridad.com>
2011-05-19 18:30:16 +0200

    [2]More work on custom events.

---
+++ addition/rdlo/scriptdev2_orgrimmar_event.sql
@@ -0,0 +1,18 @@
+REPLACE INTO `script_texts` (entry, content_default, `type`) VALUES
+-- Bolvar Fordragon
+(-300000, "¡Soldados de Ventormenta y héroes de la Alianza! Tras estos últimos días de lucha, hemos conseguido llegar hasta las mismísimas puertas de la capital de la Horda. ¡Y hemos venido para quedarnos o morir!", 1),
+(-300001, "Muchos estáis cansados o desanimados. Pero os pido que hoy hagáis el esfuerzo final para romper las defensas del enemigo y forzar a su líder a la rendición... ¡o a la muerte!", 1),
+(-300002, "¡No permitáis que las muertes de todos los soldados, compañeros y amigos que han caído en la batalla sean en vano! ¡Vamos a demostrar a esos bastardos que podemos derrotarlos en su propio territorio!", 1),
+(-300003, "Intentaremos entrar por su puerta principal. Hemos de avanzar hasta alcanzar la plaza central de Orgrimmar. Una vez allí, aseguraremos la entrada a la ciudad para que puedan entrar refuerzos.", 1),
+(-300004, "¡Por nuestros caídos! ¡Por la gloria! ¡Por la Alianza! ¡Adelante, héroes!", 1),
+(-300005, "¡Debemos asegurar la entrada de la ciudad! ¡No cedáis!", 1),
+(-300006, "¡Tened cuidado, héroes! Este tunel es un peligroso atolladero. ¡No deis ni un paso atrás!", 1),
+(-300007, "Al fin hemos alcanzado la plaza principal de la ciudad. ¡Debemos tomarla para asegurar la victoria! ¡Un último esfuerzo, soldados!", 1),
+(-300008, "¡Avanzad, soldados! ¡Entremos en el tunel!", 1);
+(-300009, "¡Estamos muy cerca! ¡Seguidme hasta la plaza central de la ciudad!", 1),
+-- Alliance Soldiers
+(-300010, "¡Por la Alianza!", 1),
+(-300011, "¡La Horda pagará por todo lo que nos ha hecho!", 1),
+(-300012, "¡Por Ventormenta!", 1),
+(-300013, "¡Adelante!", 1),
+(-300014, "¡Bien dicho!", 1);
